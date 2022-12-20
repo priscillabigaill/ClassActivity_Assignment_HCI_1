7067640286a545c397dfc5df3157e82d25f38e17
Abigail <91783712+priscillabigaill@users.noreply.github.com>
2022-12-20 22:01:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //Please put your answer here
+$(function() {
+    $('.signup-show').click(function(){
+      $('#signup-modal').fadeIn();
+    });
+
+    $('#close-modal').click(function(){
+      $('#signup-modal').fadeOut();
+    });
+});
